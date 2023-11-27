ceea6a4f2120f5941adb6d3e7d5835581f12ec30
RandySteven <randysteven12@gmail.com>
2023-11-27 22:56:38 +0700

    add unit test

--- handler/auth_handler_test.go
+++ handler/auth_handler_test.go
@@ -1,11 +1,14 @@
 package handler_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/handler"
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/mocks"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -28,7 +31,17 @@ func TestAuthHandler(t *testing.T) {
 
 func (suite *AuthHandlerTestSuite) TestLoginAuth() {
 	suite.Run("should return 200 when success to login", func() {
+		suite.authUseCase.On("LoginUserByEmail", mock.Anything, mock.Anything, mock.Anything).
+			Return("token", nil)
 
+		req, _ := http.NewRequest("POST", "/login", nil)
+		w := httptest.NewRecorder()
+
+		c, _ := gin.CreateTestContext(w)
+		c.Request = req
+
+		suite.router.POST("/login", suite.authHandler.LoginUser)
+		suite.Equal(http.StatusOK, w.Code)
 	})
 }
 
