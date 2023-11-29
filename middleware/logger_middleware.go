fcc72ebec8c0dfaf075127f94a76a8f476392cf2
RandySteven <randysteven12@gmail.com>
2023-11-29 14:59:08 +0700

    improvement logger time

--- middleware/logger_middleware.go
+++ middleware/logger_middleware.go
@@ -1,13 +1,43 @@
 package middleware
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"time"
+
 	"git.garena.com/sea-labs-id/bootcamp/batch-02/shared-projects/library-api/logger"
 	"github.com/gin-gonic/gin"
 )
 
+func readBody(reader io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	s := buf.String()
+	return s
+}
+
 func LoggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		args := make(map[string]interface{})
+		if c.Request.Body != nil {
+			buf, _ := ioutil.ReadAll(c.Request.Body)
+			rdr := ioutil.NopCloser(bytes.NewBuffer(buf))
+			c.Request.Body = rdr
+			args["request_body"] = c.Request.Body
+			// args["method"] = c.Request.Method
+			// args["status"] = c.Writer.Status()
+			// args["uri"] = c.Request.RequestURI
+		}
+
+		logger.Log.WithFields(args).Info("Request")
+
+		startTime := time.Now()
+
 		c.Next()
+
+		endTime := time.Now()
 
 		err := c.Errors.Last()
 		if err == nil {
@@ -15,10 +45,11 @@ func LoggerMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		args := make(map[string]interface{})
+		args = make(map[string]interface{})
 		args["method"] = c.Request.Method
 		args["uri"] = c.Request.RequestURI
 		args["status"] = c.Writer.Status()
+		args["latency"] = endTime.Sub(startTime).Seconds()
 
 		logger.Log.WithFields(args).Error(err)
 
